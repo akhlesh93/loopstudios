6fab83e33105299211ed6f392aa2eb44d39ff8fd
Akhlesh kumar <akhlesh011193@gmail.com>
2022-10-08 21:00:43 +0530

    A Tailwind CSS Website Project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				josefin: ['Josefin Sans', 'sans-serif'],
+				alata: ['Alata', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
